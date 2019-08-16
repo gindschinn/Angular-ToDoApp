3799e3fd276c1ccb42cb86a8441f968049d03003
gindschinn <48677094+gindschinn@users.noreply.github.com>
2019-08-16 22:58:41 +0200

    Todo App
    Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { TodoComponent } from './_template/todo/todo.component';
+import { TodoFormComponent } from './_template/todo-form/todo-form.component';
+import { HeaderComponent } from './_template/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    TodoComponent,
+    TodoFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
